f8d5dbcbe6fdc19a6499815be187ba3eb23f82ae
saghya <saghyattila77@gmail.com>
2022-10-16 12:20:53 +0200

    deprecated

--- .config/nvim/lua/user/lsp.lua
+++ .config/nvim/lua/user/lsp.lua
@@ -4,7 +4,7 @@ local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
     return
 end
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 -- lsp setup
 local status_ok, lspconfig = pcall(require, "lspconfig")
